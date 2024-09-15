a6443bd5ac6ca3223c0b456f2680b4099ca693c4
i-icc <ichi623110@gmail.com>
2024-09-16 01:16:51 +0900

    add emoji

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div>
-          現在作成中です🥺🥺
+          現在作成中です🥺🥺🥺
         </div>
       </main>
     </div>
