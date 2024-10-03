3ad834db2084dd138290ea511f8de9f9c6830eb5
i-icc <ichi623110@gmail.com>
2024-10-03 22:41:53 +0900

    #20 add pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
-module.exports = {
+const nextPWA = require('next-pwa');
+
+const withPWA = nextPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    // disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = {
     images: {
         domains: ['storage.googleapis.com'],
         unoptimized: true,
     },
     trailingSlash: true,
+    reactStrictMode: true,
+    swcMinify: true,
 };
+
+module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
