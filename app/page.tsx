6e3182625a1f9d23b442d0ca3944401bdd6c5cdd
i-icc <ichi623110@gmail.com>
2024-10-04 23:52:24 +0900

    #20 インストールボタンの追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import InstallButton from '@/components/InstallButton';
 import Header from '../components/Header';
 import PodcastCard from '../components/PodcastCard';
 import Episode from '@/components/Episode';
@@ -55,13 +56,18 @@ export default async function Home() {
           <p>一駅分の移動の時間でトレンドをキャッチ！</p>
         </div>
         <h3 className='text-lg my-2'>各種 podcast でも配信中！</h3>
-        <div>
-          <ul>
-            <li><a href="https://music.amazon.co.jp/podcasts/2c53d14b-5c3b-463a-b451-66733ddb3784/ai-cast" target='_blank'>Amazon podcast</a></li>
-            <li><a href="https://podcasts.apple.com/us/podcast/ai-cast/id1767191915" target='_blank'>Apple podcast</a></li>
-            <li><a href="https://open.spotify.com/show/0SeH7yG5doIsdypO1zqUd3?si=cISqNAMLRvedlqpRSr9-Pw" target='_blank'>Spotify podcast</a></li>
-            <li><a href="https://www.youtube.com/playlist?list=PLhnPyWxtaPqx83w7OTDK6msSbP2x0ca3L" target='_blank'>Youtube podcast</a></li>
-          </ul>
+        <div className='left-4 right-4 flex justify-between items-center'>
+          <div>
+            <ul>
+              <li><a href="https://music.amazon.co.jp/podcasts/2c53d14b-5c3b-463a-b451-66733ddb3784/ai-cast" target='_blank'>Amazon podcast</a></li>
+              <li><a href="https://podcasts.apple.com/us/podcast/ai-cast/id1767191915" target='_blank'>Apple podcast</a></li>
+              <li><a href="https://open.spotify.com/show/0SeH7yG5doIsdypO1zqUd3?si=cISqNAMLRvedlqpRSr9-Pw" target='_blank'>Spotify podcast</a></li>
+              <li><a href="https://www.youtube.com/playlist?list=PLhnPyWxtaPqx83w7OTDK6msSbP2x0ca3L" target='_blank'>Youtube podcast</a></li>
+            </ul>
+          </div>
+          <div>
+            <InstallButton />
+          </div>
         </div>
         <div className='flex-1 text-center border-t border-gray-500 my-3 p-3'>
           © 2024 <a href="https://i-icc.github.io/" target='_blank'>某猫/i-icc</a>. All rights reserved.
---
+++ components/InstallButton.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+interface Props {
+    message?: string;
+    disabledMessage?: string;
+}
+
+interface BeforeInstallPromptEvent extends Event {
+    prompt: () => void;
+    userChoice: Promise<{ outcome: "accepted" | "dismissed"; platform: string }>;
+}
+
+const InstallButton = ({
+    message = "今すぐインストール！",
+    disabledMessage = "インストール\nありがとうございます！",
+}: Props) => {
+    const [deferredPrompt, setDeferredPrompt] =
+        useState<BeforeInstallPromptEvent | null>(null);
+    const [isInstallable, setIsInstallable] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("beforeinstallprompt", (e: Event) => {
+            setDeferredPrompt(e as BeforeInstallPromptEvent);
+            setIsInstallable(true);
+        });
+    }, []);
+
+    const onInstallClick = async () => {
+        if (!deferredPrompt) {
+            return;
+        }
+
+        deferredPrompt.prompt();
+        const choiceResult = await deferredPrompt.userChoice;
+        if (choiceResult.outcome === "accepted") {
+            setDeferredPrompt(null);
+            setIsInstallable(false);
+        } else {
+            setIsInstallable(true);
+        }
+    };
+
+    return (
+        <button
+            onClick={() => void onInstallClick()}
+            className="rounded-full themaBgL notThemaC px-5 py-2 font-semibold no-underline"
+            disabled={!isInstallable}
+        >
+            {isInstallable ? message : disabledMessage}
+        </button>
+    );
+};
+
+export default InstallButton;
